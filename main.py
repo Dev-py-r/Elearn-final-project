cceec359fc3dd2ab1e65f17541e0d6c5851a063b
Dev <prasad.shetty809@gmail.com>
2021-03-23 19:01:15 +0530

    add comment

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sklearn.preprocessing import StandardScaler
 from datetime import datetime
+import joblib
+import numpy as np
+
 
 
 app = Flask(__name__,template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///back.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+model = joblib.load(r'C:\Users\prasa\Desktop\Excelr\j_svm')
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(200), nullable=False)
+    Predict = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
@@ -19,19 +26,53 @@ class Todo(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=='POST':
+    if request.method =='POST':
         Name = request.form['Name']
-        # Gender = request.form['Gender']
-        # Married = request.form['Married']
-        # Dependents = request.form['Dependents']
-        # Education = request.form['Education']
-        # Self_Employed = request.form['Self_Employed']
-        # Credit_History = request.form['Credit_History']
-        # Property_Area = request.form['Property_Area']
-        # Total_Income = request.form['Total_Income']
-        # EMI = request.form['EMI']
-        # Balance_Income = request.form['Balance_Income']
-        todo = Todo(Name=Name)
+        if(Name==""):
+            Name = 'Unknown'
+        Gender = int(request.form['Gender'])
+        if(Gender==""):
+            Gender = 1
+        Married = int(request.form['Married'])
+        if (Married == ""):
+            Married = 1
+        Dependents = int(request.form['Dependents'])
+        if (Dependents == ""):
+            Dependents = 0
+        Education = int(request.form['Education'])
+        if (Education == ""):
+            Education = 0
+        Self_Employed = int(request.form['Self_Employed'])
+        if (Self_Employed == ""):
+            Self_Employed = 1
+        Credit_History = int(request.form['Credit_History'])
+        if (Credit_History == ""):
+            Credit_History = 1
+        Property_Area = int(request.form['Property_Area'])
+        if (Property_Area == ""):
+            Property_Area = 2
+        Applicant_Income = int(request.form['Applicant_Income'])
+        if (Applicant_Income == "" or Applicant_Income > 9357):
+            Applicant_Income = 9357
+        Coapplicant_Income = int(request.form['Coapplicant_Income'])
+        if (Coapplicant_Income == "" or Coapplicant_Income > 3750):
+            Coapplicant_Income = 3750
+        Loan_Amount = int(request.form['Loan_Amount'])
+        if (Loan_Amount == "" or Loan_Amount > 228):
+            Loan_Amount = 228
+        Loan_Amount_Term = int(request.form['Loan_Amount_Term'])
+        if (Loan_Amount_Term == "" or Loan_Amount_Term > 360 or Loan_Amount_Term < 12):
+            Loan_Amount_Term = 360
+
+        scaler = StandardScaler()
+        trans = scaler.fit_transform(np.array([[Gender, Married, Dependents, Education, Self_Employed, Credit_History, Property_Area, Applicant_Income,Coapplicant_Income, Loan_Amount, Loan_Amount_Term]]))
+        predict  = model.predict(trans)
+        if (predict == 'Y'):
+            predict="Yes"
+        else:
+            predict="No"
+
+        todo = Todo(Name=Name,Predict=predict)
         db.session.add(todo)
         db.session.commit()
 
@@ -48,3 +89,8 @@ def delete(sno):
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
+
+
+    #from main import db
+    #db.create_all
+    #exit()

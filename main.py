ce8c828aac7ea2bbb735952a83cc45a5373ea278
Dev-py-r <63553264+Dev-py-r@users.noreply.github.com>
2021-03-29 22:40:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///back.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-model = joblib.load(r'C:\Users\prasa\Desktop\Excelr\j_clf')
+model = joblib.load(r'.\j_clf')
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
